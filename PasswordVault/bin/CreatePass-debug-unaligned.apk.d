7a3c7914922ad168fd328dfe5a7a16830a7f47a1
Brian Leibreich <leibreichb1@gmail.com>
2012-04-13 12:27:46 -0400

    fixed bugs

--- PasswordVault/bin/CreatePass-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/Users/newkirkj1/Documents/workspace/Password-Vault/PasswordVault/bin/CreatePass-debug-unaligned.apk : \
-/Users/newkirkj1/Documents/workspace/Password-Vault/PasswordVault/bin/CreatePass.ap_ \
-/Users/newkirkj1/Documents/workspace/Password-Vault/PasswordVault/bin/classes.dex \
